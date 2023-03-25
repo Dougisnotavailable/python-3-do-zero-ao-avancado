e34ea3341065e55e3619bd9ac0cc46439138537d
Dougisnotavailable <96440626+Dougisnotavailable@users.noreply.github.com>
2023-03-25 15:37:18 -0300

    Add files via upload

---
+++ aula30_boaspraticas.py
@@ -0,0 +1,27 @@
+"""
+CONSTANTE = "Variáveis que não vão mudar (capslock)
+Muitas condições no mesmo if (ruim)
+...... <- contagem de complexidade (ruim)
+Simples é melhor do que complexo (zen)
+"""
+
+velocidade = 61 # velocidade atual do carro
+local_carro = 101 #local em que o carro está na estrada
+
+RADAR_1 = 61 # velocidade máxima do radar 1 
+LOCAL_1 = 100 # local onde está o radar 1
+RADAR_RANGE = 1 # distância que o radar pega
+
+vel_carro_pass_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+    local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
+
+if vel_carro_pass_radar_1:
+    print('Velocidade carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print('Carro passou radar 1')
+
+if carro_multado_radar_1:
+    print('carro multado em radar 1')
