4e905d3dcaa60ef85d75ea398fa683f5c1235a64
Dougisnotavailable <96440626+Dougisnotavailable@users.noreply.github.com>
2023-03-26 14:51:36 -0300

    Add files via upload

---
+++ 59while.py
@@ -0,0 +1,17 @@
+"""
+Repetições
+while
+Executa enquanto condição for verdadeira
+
+"""
+
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome? ')
+    print(f'Seu nome é {nome}')
+
+    if nome ==  'sair':
+        break
+
+print('Cabô milho cabô piroca')
