2217a26d45f5d617b02389474cad94dc631d8d86
Douglas Henrique da Silva <96440626+Dougisnotavailable@users.noreply.github.com>
2023-05-31 12:46:36 -0300

    Update 94metodos_str.py

--- 94metodos_str.py
+++ 94metodos_str.py
@@ -3,7 +3,7 @@ split e join com list e str
 split - divide uma string (list)
 join - une uma string
 """
-frase = '   Olha só que   , coisa interessante          '
+frase = '   Olha só que   , coisa interessante         '
 lista_frases_cruas = frase.split(',')
 
 lista_frases = []
